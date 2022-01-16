6652e751c61aac105304d4071dd62448f0649990
sharmila <sharru365@gmail.com>
2022-01-16 11:19:56 +0530

    first commit

--- projects/shopping/src/app/app.module.ts
+++ projects/shopping/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { LoginComponent} from './components/login.component';
 import {RegisterComponent} from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
